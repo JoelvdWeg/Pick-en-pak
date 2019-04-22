f4fe85f4c52da1e7b141e4120a3dfc64ff70c42f
Douwe de Vries <douwe.de.vries@kpnmail.nl>
2019-04-22 13:29:13 +0200

    Firstfit en Bestfit
    Het is mogelijk om te switchen tussen firstfit en bestfit zodat we kunnen kijken wat het beste werkt.

--- BPP.java
+++ BPP.java
@@ -11,51 +11,87 @@ public class BPP {
         volgorde = new ArrayList<>();
         this.items = items;
 
-        bepaalVolgorde();
-    }
-
-    private void bepaalVolgorde() {
         try {
-            for (int i = 0; i < items.size(); i++) {
-                Doos geselecteerd = null;
-
-                // Mogelijke dozen zoeken
-                ArrayList<Doos> mogelijkeDozen = new ArrayList<>();
-                for (Doos doos: dozen) {
-                    if (doos.getRuimte() >= items.get(i).getGrootte()) {
-                        mogelijkeDozen.add(doos);
-                    }
-                }
-
-                if (mogelijkeDozen.size() == 0) { // Als het opject in geen enkele doos past
-                    geselecteerd = new Doos(items.get(i));
-                    dozen.add(geselecteerd);
-                }
-                else { // Als het object wel in één of meerdere dozen past.
-                    // Kleinste doos uit mogelijke dozen.
-                    Doos kleinsteDoos = mogelijkeDozen.get(0);
-                    for (Doos doos : mogelijkeDozen) {
-                        if (doos.getRuimte() < kleinsteDoos.getRuimte()) {
-                            kleinsteDoos = doos;
-                        }
-                    }
-
-                    // object in kleinste doos stoppen
-                    geselecteerd = kleinsteDoos;
-                    geselecteerd.aanvullen(items.get(i));
-                }
-
-                volgorde.add(geselecteerd);
-
-                // Huidige ronde printen
-                printRonde(i, geselecteerd);
-            }
-
-            printVolgorde();
+            // Algortiem kiezen
+            //bepaalVolgordeBestfit();
+            bepaalVolgordeFirstfit();
         }
         catch (NullPointerException npe) {
             System.out.println("Geen items gegeven aan het BPP-algoritme.");
         }
+    }
+
+    private void bepaalVolgordeBestfit() {
+        System.out.println("BESTFIT --------------------\n");
+
+        for (int i = 0; i < items.size(); i++) {
+            Doos geselecteerd;
+
+            // Mogelijke dozen zoeken
+            ArrayList<Doos> mogelijkeDozen = new ArrayList<>();
+            for (Doos doos: dozen) {
+                if (doos.getRuimte() >= items.get(i).getGrootte()) {
+                    mogelijkeDozen.add(doos);
+                }
+            }
+
+            if (mogelijkeDozen.size() == 0) { // Als het opject in geen enkele doos past
+                geselecteerd = new Doos(items.get(i));
+                dozen.add(geselecteerd);
+            }
+            else { // Als het object wel in één of meerdere dozen past.
+                // Kleinste doos uit mogelijke dozen.
+                Doos kleinsteDoos = mogelijkeDozen.get(0);
+                for (Doos doos : mogelijkeDozen) {
+                    if (doos.getRuimte() < kleinsteDoos.getRuimte()) {
+                        kleinsteDoos = doos;
+                    }
+                }
+
+                // object in kleinste doos stoppen
+                geselecteerd = kleinsteDoos;
+                geselecteerd.aanvullen(items.get(i));
+            }
+
+            // Toevoegen aan volgorde
+            volgorde.add(geselecteerd);
+
+            // Huidige ronde printen
+            printRonde(i, geselecteerd);
+        }
+
+        printVolgorde();
+    }
+
+    public void bepaalVolgordeFirstfit() {
+        System.out.println("FIRSTFIT --------------------\n");
+
+        for (int i = 0; i < items.size(); i++) {
+            Doos geselecteerd = null;
+
+            // Bij elke doos kijken of er ruimte is. Zo ja: selecteer en stop de for loop.
+            for (Doos doos : dozen) {
+                if (doos.getRuimte() >= items.get(i).getGrootte()) {
+                    geselecteerd = doos;
+                    geselecteerd.aanvullen(items.get(i));
+                    break;
+                }
+            }
+
+            // Nieuwe doos maken als er nog geen een geselecteerd is.
+            if (geselecteerd == null) {
+                geselecteerd = new Doos(items.get(i));
+                dozen.add(geselecteerd);
+            }
+
+            // Toevoegen aan volgorde
+            volgorde.add(geselecteerd);
+
+            // Alle dozen printen
+            printRonde(i, geselecteerd);
+        }
+
+        printVolgorde();
     }
 
     public void printRonde(int i, Doos geselecteerd) {
