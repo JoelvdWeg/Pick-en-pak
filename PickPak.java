f516e346aab15a8154af67208ab52a4df4ac6585
lukas-ve <lukasvanelten@gmail.com>
2019-05-07 10:11:05 +0200

    Items die gepickt moeten worden worden nu opgevraagd uit een txt bestand

--- PickPak.java
+++ PickPak.java
@@ -6,28 +6,17 @@ public class PickPak {
 
     private ArrayList<Doos> volgorde;
     public static ArrayList<Item> items;
-    public ArrayList<Item> pakbon;
+    //public ArrayList<Item> pakbon;
 
-    public PickPak() {
-        items = new ArrayList<>();
-        Random r = new Random();
-        int k = 0;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                items.add(new Item(new Locatie(k, new Coordinate(i, j)),r.nextInt(12)+1));
-                k++;
-            }
-        }
-        items.add(new Item(new Locatie(k, new Coordinate(4, 0))));
-
-        pakbon = new ArrayList<Item>();
-
-        for(int i = 0; i < r.nextInt(25)+1; i++){
-            pakbon.add(items.get(r.nextInt(26)));
-        }
-
+    public PickPak(ArrayList<Item> pakbon) {
         
-        
+
+        //pakbon = new ArrayList<Item>();
+
+        //for(int i = 0; i < r.nextInt(25)+1; i++){
+        //    pakbon.add(items.get(r.nextInt(26)));
+        //}
+
         BPP bpp = new BPP(pakbon);
         volgorde = bpp.getVolgorde();
         
--- run.java
+++ run.java
@@ -1,6 +1,57 @@
-public class run {
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class run {    
+    private static ArrayList<Item> pakbon;
+    
     public static void main(String[] args) {
-        PickPak pp = new PickPak();
+        maakItems();
+        
+        for(Item item: PickPak.items){
+            System.out.println(item);
+        }
+        
+        pakbon = new ArrayList<>();
+        
+        try{
+            FileReader file = new FileReader("newfile.txt");
+            Scanner parser = new Scanner(file);
+            while(parser.hasNextLine()){
+                voegToeAanPakbon(parser.nextLine());
+            }
+        } catch(FileNotFoundException fe){
+            System.out.println(fe);
+        }
+        
+        PickPak pp = new PickPak(pakbon);
+        
+        
+    }
+    
+    private static void voegToeAanPakbon(String line){
+        int nextItemID = Integer.parseInt(line);
+       
+        for(Item item: PickPak.items){
+            if(item.getLocatie().getID() == nextItemID){
+                pakbon.add(item);
+                System.out.println("Item met locatie-id" + item.getLocatie().getID() + " toegevoegd aan pakbon");
+                break;
+            }
+        }
+    }
+    
+    private static void maakItems(){
+        PickPak.items = new ArrayList<>();
+        Random r = new Random();
+        int k = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                PickPak.items.add(new Item(new Locatie(k, new Coordinate(i, j)),r.nextInt(12)+1));
+                k++;
+            }
+        }
+        PickPak.items.add(new Item(new Locatie(k, new Coordinate(4, 0))));
     }
 }
