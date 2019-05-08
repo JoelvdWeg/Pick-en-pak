35949b91057492710269e1101ce9300319b8c6b6
Jens Brinkman <jensbrinkman@home.nl>
2019-05-08 14:30:53 +0200

    Create DatabaseConn.java
    Database connectie met wideworldimporters (haalt items op)

---
+++ libraryTest/DatabaseConn.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libraryTest;
+import java.sql.*;  
+class DatabaseConn{  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/wideworldimporters","root","");  
+//here wideworldimporters is database name, root is username and password  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from stockitems");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getInt(3));  
+
+System.out.println("gelukt!");
+con.close();  
+}catch(Exception e){ System.out.println(e); System.out.println("mislukt!");}  
+}  
+}  
+
+
+/*
+
+package libraryTest;
+import java.sql.*;  
+class DatabaseConn{  
+public static void main(String args[]){  
+try{  
+Class.forName("com.mysql.jdbc.Driver");  
+Connection con=DriverManager.getConnection(  
+"jdbc:mysql://localhost:3306/sonoo","root","");  
+//here sonoo is database name, root is username and password  
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from emp");  
+while(rs.next())  
+System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+
+System.out.println("gelukt!");
+con.close();  
+}catch(Exception e){ System.out.println(e); System.out.println("mislukt!");}  
+}  
+}  
+
+
+*/
