dfe69fb5b062436d7e81c08676ae982b93f262e8
Bl0ndeSmurf <43145188+Bl0ndeSmurf@users.noreply.github.com>
2019-05-10 13:09:20 +0200

    Arduino connectie toegevoegd

--- GUI/PickFrame.java
+++ GUI/PickFrame.java
@@ -17,62 +17,110 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import arduino.*;
 
 public class PickFrame extends JFrame implements ActionListener {
-private Pick pick;
-private JTextField  jtfx, jtfy;
-private JLabel      jlx, jly, jlcontrolpanel, jlbestelling, jlstelling, jldozen; 
-private JButton     jbbevestig;
 
+	private Pick pick;
+	private JTextField jtfx, jtfy;
+	private JLabel jlx, jly, jlcontrolpanel, jlbestelling, jlstelling, jldozen;
+	private JButton jbbevestig;
 
+	private Arduino arduino;
 
-    public PickFrame() {
-        setTitle("GUI");
-        setSize(1920, 1080);
-        setLayout(new FlowLayout());
-        
-       this.pick = pick;
-        jlx = new JLabel("x-as");
-        add(jlx);
-        
-        jtfx = new JTextField(4);
-        add(jtfx);
-        
-        jly  = new JLabel ("y-as");
-        add(jly);
-        
-        jtfy = new JTextField(4);
-        add(jtfy);
-        
-        jbbevestig = new JButton ("Bevestig");
-        jbbevestig.addActionListener(this);
-        add(jbbevestig);
-        
-        setVisible(true);
-        PickPanel panel = new PickPanel();
-        add(panel);
-        
-        
-    }
+	private PortDropdownMenu pdmCOM;
+	private JButton jbRefresh;
+	private JButton jbConnect;
 
+	public PickFrame() {
+		setTitle("GUI");
+		setSize(1920, 1080);
+		setLayout(new FlowLayout());
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == jbbevestig) {
-            try {
-                Pick c = new Pick(Integer.parseInt(jtfx.getText()), Integer.parseInt(jtfy.getText()));
-                Pick.naarVakje(c); 
-            }                
-            catch (NumberFormatException nfe) {
-                    System.out.println("onjuiste invoer!");
-                }
-        repaint();
-        
-        }
-    }
+		this.pick = pick;
+
+		pdmCOM = new PortDropdownMenu();
+		add(pdmCOM);
+		pdmCOM.refreshMenu();
+
+		jbRefresh = new JButton("Refresh");
+		jbRefresh.addActionListener(this);
+		add(jbRefresh);
+
+		jbConnect = new JButton("Connect");
+		jbConnect.addActionListener(this);
+		add(jbConnect);
+
+		jlx = new JLabel("x-as");
+		jlx.setEnabled(false);
+		add(jlx);
+
+		jtfx = new JTextField(4);
+		jtfx.setEnabled(false);
+		add(jtfx);
+
+		jly = new JLabel("y-as");
+		jly.setEnabled(false);
+		add(jly);
+
+		jtfy = new JTextField(4);
+		jtfy.setEnabled(false);
+		add(jtfy);
+
+		jbbevestig = new JButton("Bevestig");
+		jbbevestig.addActionListener(this);
+		jbbevestig.setEnabled(false);
+		add(jbbevestig);
+
+		setVisible(true);
+		PickPanel panel = new PickPanel();
+		add(panel);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == jbbevestig) {
+			try {
+				Pick c = new Pick(Integer.parseInt(jtfx.getText()), Integer.parseInt(jtfy.getText()));
+//                Pick.naarVakje(c); 
+			} catch (NumberFormatException nfe) {
+				System.out.println("onjuiste invoer!");
+			}
+			repaint();
+			
+			arduino.serialWrite(jtfx.getText() + jtfy.getText());
+			
+			jtfx.setText("");
+			jtfy.setText("");
+		} else if (e.getSource() == jbRefresh) {
+			pdmCOM.refreshMenu();
+		} else if (e.getSource() == jbConnect) {
+			if (jbConnect.getText().equals("Connect")) {
+				arduino = new Arduino(pdmCOM.getSelectedItem().toString(), 9600);
+				if (arduino.openConnection()) {
+					jbConnect.setText("Disconnect");
+					pdmCOM.setEnabled(false);
+					jbRefresh.setEnabled(false);
+
+					jlx.setEnabled(true);
+					jtfx.setEnabled(true);
+					jly.setEnabled(true);
+					jtfy.setEnabled(true);
+					jbbevestig.setEnabled(true);
+				}
+			} else {
+				arduino.closeConnection();
+				jbConnect.setText("Connect");;
+				pdmCOM.setEnabled(true);
+				jbRefresh.setEnabled(true);
+
+				jlx.setEnabled(false);
+				jtfx.setEnabled(false);
+				jly.setEnabled(false);
+				jtfy.setEnabled(false);
+				jbbevestig.setEnabled(false);
+			}
+		}
+	}
 }
-
-
-
-
-
