88367d880b22c7d2a0534834c8f912a106004143
lukas-ve <lukasvanelten@gmail.com>
2019-05-22 11:00:11 +0200

    ...

--- PickPak/PickFrame.java
+++ PickPak/PickFrame.java
@@ -86,7 +86,7 @@ public class PickFrame extends JFrame implements ActionListener {
 
                         pickpak.resetRobots();
                         
-                        //panel.paintImmediately(0, 0, 1920, 1080);
+                        panel.paintImmediately(0, 0, 1920, 1080);
                     }
 
                     tekenRoute("...");
