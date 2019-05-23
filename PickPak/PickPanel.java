3fa1832109cac605ad811eecd62b24acf74a362f
lukas-ve <lukasvanelten@gmail.com>
2019-05-23 09:23:27 +0200

    ...

--- PickPak/PickPanel.java
+++ PickPak/PickPanel.java
@@ -36,21 +36,21 @@ public class PickPanel extends JPanel {
         g2.setStroke(new BasicStroke(5));
 
 //        grote vierkant
-        g.drawRect(10, 200, 1900, 990);
+        g.drawRect(10, 0, 1900, 990);
         //g.drawLine(950, 10, 950, 1000);
 //          lijn op de horizontale helft
         //g.drawLine(10, 400, 1920, 400);
-        g.drawRect(200, 250, 500, 500);
+        g.drawRect(200, 50, 500, 500);
 
 //        GRID 
         int x = 300;
         for (int i = 0; i < 4; i++) {
 
-            g.drawLine(x, 250, x, 950);
+            g.drawLine(x, 50, x, 950);
             x = x + 100;
         }
 
-        int y = 350;
+        int y = 150;
         for (int i = 0; i < 4; i++) {
 
             g.drawLine(200, y, 700, y);
@@ -59,22 +59,22 @@ public class PickPanel extends JPanel {
 
 //            DOZEN 
         g2.setStroke(new BasicStroke(2));
-        g.drawRect(1100, 300, 50, 400);
-        g.drawRect(1200, 300, 50, 400);
-        g.drawRect(1300, 300, 50, 400);
-        g.drawRect(1400, 300, 50, 400);
-        g.drawRect(1500, 300, 50, 400);
-        g.drawRect(1600, 300, 50, 400);
+        g.drawRect(1100, 100, 50, 400);
+        g.drawRect(1200, 100, 50, 400);
+        g.drawRect(1300, 100, 50, 400);
+        g.drawRect(1400, 100, 50, 400);
+        g.drawRect(1500, 100, 50, 400);
+        g.drawRect(1600, 100, 50, 400);
         
 
 
         g.setColor(Color.GRAY);
-        g.fillRect(1100, 300, 50, 400);
-        g.fillRect(1200, 300, 50, 400);
-        g.fillRect(1300, 300, 50, 400);
-        g.fillRect(1400, 300, 50, 400);
-        g.fillRect(1500, 300, 50, 400);
-        g.fillRect(1600, 300, 50, 400);
+        g.fillRect(1100, 100, 50, 400);
+        g.fillRect(1200, 100, 50, 400);
+        g.fillRect(1300, 100, 50, 400);
+        g.fillRect(1400, 100, 50, 400);
+        g.fillRect(1500, 100, 50, 400);
+        g.fillRect(1600, 100, 50, 400);
 
         g.setColor(Color.BLACK);
         g2.setStroke(new BasicStroke(4));
